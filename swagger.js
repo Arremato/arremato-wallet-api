b53db4ebe5fd72409178e271e39d7ca89aa74d3f
Euclides Silva <euclidessilva@Euclidess-MacBook-Air.local>
2025-04-23 21:43:45 -0300

    changes swagger

--- swagger.js
+++ swagger.js
@@ -1,25 +1,37 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Arremato Wallet API',
-      version: '1.0.0',
-      description: 'API para gerenciamento da carteira de imóveis',
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000/api',
-        description: 'Servidor local',
-      },
-      {
-        url: 'https://arremato-wallet-api-27b8277f775f.herokuapp.com/api',
-        description: 'Servidor Homolog',
-      },
-    ],
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Arremato Wallet API',
+    version: '1.0.1',
+    description: 'API para gerenciamento de propriedades.',
   },
+  servers: [
+    {
+      url: 'http://localhost:3000/api',
+      description: 'Servidor local',
+    },
+  ],
+  components: {
+    securitySchemes: {
+      BearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+    },
+  },
+  security: [
+    {
+      BearerAuth: [],
+    },
+  ],
+};
+
+const options = {
+  definition: swaggerDefinition,
   apis: ['./src/routes/index.js'],
 };
 
