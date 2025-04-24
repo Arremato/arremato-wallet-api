2dbabff897bef662bfb38e12e9d67b0bf7922a2e
Euclides Silva <euclidessilva@Euclidess-MacBook-Air.local>
2025-04-24 01:01:15 -0300

    change to use CORS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import setupSwagger from '../swagger.js';
 import {setRoutes} from './routes/index.js';
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://arremato-front-b3e87156c692.herokuapp.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 setRoutes(app);
 setupSwagger(app);
 
